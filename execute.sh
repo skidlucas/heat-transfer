23e88e5131d3ebf1651c996d6ecc7a3a73afafd7
Lucas MARTINEZ <martinez.lucas@etu.unice.fr>
2016-02-03 15:35:03 +0100

    modif script

--- execute.sh
+++ execute.sh
@@ -11,12 +11,24 @@ echo " - avec une plaque de taille 16*16, puis 128*128 et enfin 256*256 (-s 0, -
 echo " - avec 4 threads puis avec 64 threads (-t 1, -t 3)"
 echo " - 10000 itérations (-i 10000)"
 echo " - en mesurant le temps CPU consommé (-m)"
-echo "-----------------------------------------------"
 
-for i in {0..5}
+echo "-------------------------------------------------------"
+echo -e "\t\tEXECUTION DU SCENARIO 0"
+echo "-------------------------------------------------------"
+for j in 0 2 4
 do
+	let "s = $j + 4"
+	for ((x=1, size=2; x<s; x++)); do ((size *= 2)); done
+	echo -e "\t*** Plaque de $size*$size, avec 1 thread ***" 
+	#echo "s = $j " #verif
+	./heatTransfer  -i 10000 -e $i -s $j -t 0 -m
+done
+
+for i in {1..5}
+do
+	echo "-------------------------------------------------------"
 	echo -e "\t\tEXECUTION DU SCENARIO $i"
-	echo -e "-------------------------------------------------------"
+	echo "-------------------------------------------------------"
     for j in 0 2 4
     do
     	let "s = $j + 4"
