123614c238b8096633e14072ed40a5dbeb384d1c
Lucas MARTINEZ <martinez.lucas@etu.unice.fr>
2016-02-03 13:19:30 +0100

    script ok (je pense)

--- execute.sh
+++ execute.sh
@@ -13,5 +13,21 @@ echo " - 10000 itérations (-i 10000)"
 echo " - en mesurant le temps CPU consommé (-m)"
 echo "-----------------------------------------------"
 
-#./heatTransfer -i 10000 -e 0 -s 0 -t 1 -m
-#trouver comment exécuter une après l'autre les différentes versions
+for i in {0..5}
+do
+	echo -e "\t\tEXECUTION DU SCENARIO $i"
+	echo -e "-------------------------------------------------------"
+    for j in 0 2 4
+    do
+    	let "s = $j + 4"
+    	for ((x=1, size=2; x<s; x++)); do ((size *= 2)); done
+    	for k in 1 3
+    	do
+    		for ((x=1, threads=4; x<$k; x++)); do ((threads *= 4)); done
+
+    		echo -e "\t*** Plaque de $size*$size, avec $threads threads ***" 
+    		#echo "s = $j et t = $k" #pour vérif que les args sont bons
+    		./heatTransfer  -i 10000 -e $i -s $j -t $k -m
+    	done
+    done
+done
